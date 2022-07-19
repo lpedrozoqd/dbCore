d21b490d36e437bf3da297edc51b298dc36d1f0a
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-07-19 15:50:50 -0500

    Creación de repositorio

---
+++ src/test/java/edu/clarape/al/dbCore/DbCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.clarape.al.dbCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
